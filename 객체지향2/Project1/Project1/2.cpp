75fa2655f7cd3a986af5c1522eb26cf9243a82a0
원치현 <58906858+chihyeonWON@users.noreply.github.com>
2023-01-23 13:15:10 +0900

    Add files via upload

---
+++ 객체지향2/Project1/Project1/2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape {
+private:
+	int x, y;
+public:
+	Shape() {
+		cout << "Shape 생성자" << endl;
+	}
+	~Shape() {
+		cout << "Shape 소멸자" << endl;
+	}
+	Shape(int xloc, int yloc) {
+		xloc = x;
+		yloc = y;
+		cout << "Shape 생성자" << endl;
+	}
+};
+
+class Rectangle : public Shape {
+private:
+	int width, height;
+public:
+	Rectangle(int x, int y, int w, int h) : Shape(x, y) {
+		width = w;
+		height = h;
+		cout << "Rectangle 생성자" << endl;
+	}
+	~Rectangle() {
+		cout << "Rectangle 소멸자" << endl;
+	}
+};
+
+int main()
+{
+	Rectangle r(0, 0, 100, 100);
+	return 0;
+}
---
+++ 객체지향2/Project1/Project1/7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int speed = 60;
+	double time = 2.5;
+	double distance = speed * time;
+	cout << "이동한 거리는 " << distance << "km 입니다.\n";
+}
